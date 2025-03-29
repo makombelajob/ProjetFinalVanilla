44e2a733128f67417a4f37af9e140e934d560c87
makombelajob <50692272+makombelajob@users.noreply.github.com>
2025-03-29 13:22:56 +0100

    formContact ready

--- Js/script.js
+++ Js/script.js
@@ -40,5 +40,106 @@ function slide() {
 
     const intervalle = setInterval(forward, 10000);
 }
+function formContact() {
+
+    let emailValid = false;
+    let sujetValid = false;
+    let msgValid = false;
+    let rgpdValid = false;
+    const submitBtn = document.querySelector("#submit");
+
+    function allValid() {
+        submitBtn.disabled = !(emailValid && sujetValid && msgValid && rgpdValid);
+    }
+
+    const emailField = document.querySelector("#email");
+    emailField.addEventListener("change", function() {
+        const valid = this.nextElementSibling;
+        const invalid = this.nextElementSibling.nextElementSibling;
+        const feedback = this.parentElement.nextElementSibling;
+
+        const regex = /\S+@\S+\.\S+/;
+        if(!regex.test(emailField.value)){
+            invalid.style.display = "block";
+            feedback.style.display = "block";
+            emailValid = false;
+        }else{
+            invalid.style.display = "none";
+            feedback.style.display = "none";
+            valid.style.display = "block";
+            emailValid = true;
+        }
+        allValid();
+    });
+
+    const sujetField = document.querySelector("#sujet");
+    sujetField.addEventListener("change", function() {
+
+        const valid = this.nextElementSibling;
+        const invalid = this.nextElementSibling.nextElementSibling;
+        const feedback = this.parentElement.nextElementSibling;
+
+       if(this.value.length < 5) {
+           invalid.style.display = "block";
+           feedback.style.display = "block";
+           sujetValid = false;
+       }else{
+           invalid.style.display = "none";
+           feedback.style.display = "none";
+           valid.style.display = "block";
+           sujetValid = true;
+       }
+       allValid();
+    });
+
+    const msgField =  document.querySelector("#message");
+    msgField.addEventListener("change", function() {
+
+        const valid = this.nextElementSibling;
+        const invalid = this.nextElementSibling.nextElementSibling;
+        const feedback = this.parentElement.nextElementSibling;
+        if(this.value.length < 50) {
+            invalid.style.display = "block";
+            feedback.style.display = "block";
+            msgValid = false;
+        }else{
+            invalid.style.display = "none";
+            feedback.style.display = "none";
+            valid.style.display = "block";
+            msgValid = true;
+        }
+        allValid();
+    });
+
+    const rgpdField = document.querySelector("#rgpd");
+    rgpdField.addEventListener("change", function() {
+        const feedback = this.nextElementSibling.nextElementSibling;
+       if(!this.checked) {
+           feedback.style.display = "block";
+           rgpdValid = false;
+       }else{
+           feedback.style.display = "none";
+           rgpdValid = true;
+       }
+       allValid();
+    });
+
+    const formContact = document.querySelector("#formContact");
+    formContact.addEventListener("submit", function(e){
+       e.preventDefault();
+       if(emailValid && sujetValid && msgValid && rgpdValid) {
+           alert("Message envoyer");
+           location.href = "index.html";
+       }else{
+           const eventErrors = new Event("change");
+           emailField.dispatchEvent(eventErrors);
+           sujetField.dispatchEvent(eventErrors);
+           msgField.dispatchEvent(eventErrors);
+           rgpdField.dispatchEvent(eventErrors);
+       }
+
+    });
+}
+formContact();
 burger();
 slide();
